c8f0586f792e3419370789bfff33738406c687b5
NationofFoxes <stuart.william.auld@gmail.com>
2021-01-25 22:44:58 +0900

    Let me know what you think, it's still in progress

---
+++ Questions.py
@@ -0,0 +1,64 @@
+qanda = {"1+1=":2,"1+2=":3, "2+2=":4}
+
+class Questions:
+    nQuestions = 0
+    nCorrect = 0
+
+    def __init__(self, query, correct_answer):
+        self.query = query
+        self.correctAnswer = correct_answer
+
+
+    def check(self):
+        answer = self.ask()
+        if answer == self.correctAnswer:
+            print("Correct!")
+            self.nCorrect += 1
+        else:
+            print("Incorrect. ")
+
+    def ask(self):
+        self.nQuestions += 1
+        print(self.query)
+        answer = input("")
+        answer = answer.upper()
+        return answer
+
+
+p1 = Questions("盲目", "Blindness")   #test
+Questions.ask(p1)
+print(Questions.nCorrect, "/", Questions.nQuestions)
+
+# seeing as in the future there may be more than 1 type of question,
+#I wanted to have msq inherit the
+class MultipleChoiceQuestion(Questions):
+
+    def __init__(self):
+        pass
+
+    def msq (self, a, b, c, d, e):
+        pass
+
+
+
+#recycle bin:
+
+#for i, j in qanda.items():
+#    print(i)
+#    answer = input("Please type your answer: ")
+#    if int(answer) == j:
+#        print("Correct!")
+#        Questions.nCorrect += 1
+#    else:
+#        print("Incorrect.")
+#        print(answer, j)
+#    Questions.nQuestions += 1
+#print("Correct answers: ", Questions.nCorrect, "/", Questions.nQuestions)
+
+
+
+
+
+
+
+
